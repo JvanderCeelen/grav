c2f501a6dfbd25bbc791b5f363eba5f77fd8e598
Jonathan van der Ceelen <j.vanderceelen@minddistrict.com>
2020-01-23 13:59:00 +0100

    .gitignore fix

--- cache/compiled/files/ddf3ec9554aac23be3d82373a68bf080.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/jvanderceelen/source/susan/user/config/media.yaml',
-    'modified' => 1566376870,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/fb4e603f03322fb3cf6bda59e67cc0a9.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/home/jvanderceelen/source/susan/user/config/security.yaml',
-    'modified' => 1566312482,
-    'data' => [
-        'salt' => 'syrEwlXDa3UBLx'
-    ]
-];
